25fb7c43e2de385a86b824728b476dbe438a6fb2
Sarah  Elysayev <sarahel@dsigpu01.local>
2021-05-23 19:48:37 +0300

    Added code to extract one test sound

---
+++ final_engineering_project/test/TestDataset.py
@@ -0,0 +1,116 @@
+import os
+from typing import Any, Dict, List, Optional
+import torch
+import torchaudio  # type: ignore
+import pandas as pd  # type: ignore
+from torch.utils.data import Dataset
+from final_engineering_project.train.OVectorUtility import OVectorUtility
+from final_engineering_project.properties import test_path
+from final_engineering_project.data.mixure_dataset import MixureDataset
+
+_csv_file = os.path.join(test_path, "data.csv")
+_root_dir = os.path.join(test_path, "files")
+
+SampleType = Dict[str, Any]
+EffectsInitType = Optional[List[List[str]]]
+
+
+class TestDataset(Dataset[SampleType]):
+    def __init__(
+        self,
+        o_vector_utility: OVectorUtility,
+        min_mixure: int,
+        max_mixure: int,
+        device: Any = None,
+        from_fs: bool = True,
+        length: int = 0,
+    ) -> None:
+        self._device = device
+        self._o_vector_utility = o_vector_utility
+        self._from_fs = from_fs
+        self._length = length
+
+        if from_fs:
+            self._csv = pd.read_csv(_csv_file)
+        else:
+            self._mixure_dataset = MixureDataset(
+                train_size=0,
+                test_size=length,
+                device=device,
+                min_mixure=min_mixure,
+                max_mixure=max_mixure,
+            )
+
+    def __len__(self) -> int:
+        length = self._length
+
+        if self._from_fs:
+            csv_len = len(self._csv)
+
+            if length == 0 or length > csv_len:
+                return csv_len
+
+        return length
+
+    def __getitem__(self, idx: Any) -> SampleType:
+        if torch.is_tensor(idx):  # type: ignore
+            idx = idx.tolist()
+
+        if self._from_fs:
+            wav_path = os.path.join(_root_dir, self._csv.iloc[idx, 0])
+
+            waveform, rate = torchaudio.sox_effects.apply_effects_file(
+                path=wav_path,
+                channels_first=True,
+                effects=[],
+            )
+
+            events_wav_paths = [
+                os.path.join(_root_dir, x) for x in self._csv.iloc[idx, 1].split("|")
+            ]
+            events_labels = self._csv.iloc[idx, 2].split("|")
+            events_wavs = [
+                torchaudio.sox_effects.apply_effects_file(
+                    path=event_wav_path,
+                    channels_first=True,
+                    effects=[],
+                )
+                for event_wav_path in events_wav_paths
+            ]
+
+            sample = {
+                "waveform": waveform.to(self._device) if self._device else waveform,
+                "events": [
+                    {
+                        "waveform": event_wav[0].to(self._device)
+                        if self._device
+                        else event_wav[0],
+                        "o_vector": self._o_vector_utility.get_o_vector_by_label(
+                            events_labels[i]
+                        ),
+                    }
+                    for i, event_wav in enumerate(events_wavs)
+                ],
+            }
+
+            return sample
+
+        mixure_sample = self._mixure_dataset[idx]
+        waveform = mixure_sample["waveform"]
+        events_wavs = mixure_sample["events"]
+        events_labels = mixure_sample["labels"]
+
+        sample = {
+            "waveform": mixure_sample["waveform"],
+            "events": [
+                {
+                    "waveform": event_wav,
+                    "o_vector": self._o_vector_utility.get_o_vector_by_label(
+                        events_labels[i]
+                    ),
+                }
+                for i, event_wav in enumerate(events_wavs)
+            ],
+        }
+
+        return sample
--- final_engineering_project/test/test.py
+++ final_engineering_project/test/test.py
@@ -1,12 +1,20 @@
+import os
+from os import path
+from shutil import rmtree
 import torch
+import torchaudio
+from torch.utils.data.dataloader import DataLoader
+from final_engineering_project.test.TestDataset import TestDataset
 from final_engineering_project.train.model import Model
 from final_engineering_project.train.OVectorUtility import OVectorUtility
-from final_engineering_project.properties import model_path
+from final_engineering_project.properties import model_path, test_path
+
+resample = 8000
 
 
 def test() -> None:
     gpu_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    # cpu_device = torch.device("cpu")
+    cpu_device = torch.device("cpu")
 
     o_vector_utility = OVectorUtility(
         device=gpu_device,
@@ -19,4 +27,50 @@ def test() -> None:
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
-    print(model)
+    test_dataset = TestDataset(
+        o_vector_utility=o_vector_utility,
+        device=gpu_device,
+        from_fs=False,
+        min_mixure=3,
+        max_mixure=3,
+        length=1,
+    )
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=1,
+        shuffle=False,
+    )
+
+    rmtree(path.join(test_path, "results"), ignore_errors=True)
+    os.makedirs(path.join(test_path, "results", "files"), exist_ok=True)
+
+    for (i, batch) in enumerate(test_dataloader):
+        y = batch["waveform"]
+
+        torchaudio.save(
+            path.join(test_path, "results", "files", "{0}.wav".format(i)),
+            y.squeeze(1).to(cpu_device),
+            resample,
+        )
+
+        for event_i, event in enumerate(batch["events"]):
+            x = event["waveform"]
+            o = event["o_vector"]
+            x_pred = model(y, o)
+            torchaudio.save(
+                path.join(
+                    test_path,
+                    "results",
+                    "files",
+                    "{0}_{1}_expected.wav".format(i, event_i),
+                ),
+                x.squeeze(1).to(cpu_device),
+                resample,
+            )
+            torchaudio.save(
+                path.join(
+                    test_path, "results", "files", "{0}_{1}.wav".format(i, event_i)
+                ),
+                x_pred.squeeze(1).to(cpu_device),
+                resample,
+            )
